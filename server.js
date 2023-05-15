1f24700b4f3abb2e0e2133e919ed0096aa78302d
Prestley <priestlypriest@gmail.com>
2023-05-15 20:27:18 +0530

    adding JS functions

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const toggle = document.querySelector()
+document.querySelector('.dark-mode-switch').onclick = () =>{
+    document.querySelector('body').classList.toggle('dark')
+    document.querySelector('body').classList.toggle('light')
+}
+
+isleapYear = (year) =>{
+    return (year % 4 === 0 && year % 100!== 0 && year % 400!==0) || ( year % 100 === 0 && year % 400 === 0)
+}
+
+getFebDays = (year) =>{
+    return isleapYear(year)? 29 : 28
+}
+
+let calender = document.querySelector('.calender')
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+generateCalender = (month, year) =>{
+    let calender_days = document.querySelector('.caleder-days')
+    let calender_header_year = document.querySelector('#year')
+    let days_of_month = 
+}
