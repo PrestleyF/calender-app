ac841b01634cc8f088bbe178a189a99a51fa5dd8
Prestley <priestlypriest@gmail.com>
2023-05-16 18:12:28 +0530

    changes to js code

--- server.js
+++ server.js
@@ -14,21 +14,23 @@ getFebDays = (year) =>{
 let calender = document.querySelector('.calender')
 
 const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+let month_picker = document.querySelector('#month-picker')
 
 generateCalender = (month, year) =>{
-    let calender_days = document.querySelector('.caleder-days')
+    let calender_days = document.querySelector('.calender-days')
 
     calender_days.innerHTML = ''
 
     let calender_header_year = document.querySelector('#year')
+
     let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    let currentDate = newDate()
+    let currentDate = new Date()
 
     month_picker.innerText = month_names[month]
     calender_header_year.innerHTML = year
 
-    let first_day = newDate(month, year)
+    let first_day = new Date(year, month,1)
 
     calender_header_year.innerText = year
 
@@ -47,11 +49,47 @@ generateCalender = (month, year) =>{
            calender_days.appendChild(day)
     }
 }
+ let month_list = 
 
-
-let currentDate = newDate()
+let currentDate = new Date()
 
 let currentMonth = {value:currentDate.month()}
 let currentYear = {value:currentDate.getFullYear()}
 
 generateCalender(currentMonth.value, currentYear.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
