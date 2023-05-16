1d2badf5eaa2c5442187b739ab1be9d49005c13a
Prestley <priestlypriest@gmail.com>
2023-05-16 17:03:55 +0530

    added some js functions

--- server.js
+++ server.js
@@ -17,6 +17,41 @@ const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'Ju
 
 generateCalender = (month, year) =>{
     let calender_days = document.querySelector('.caleder-days')
+
+    calender_days.innerHTML = ''
+
     let calender_header_year = document.querySelector('#year')
-    let days_of_month = 
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    let currentDate = newDate()
+
+    month_picker.innerText = month_names[month]
+    calender_header_year.innerHTML = year
+
+    let first_day = newDate(month, year)
+
+    calender_header_year.innerText = year
+
+    for(let i= 0; i <= days_of_month[month] + first_day.getDay() - 1; i++){
+           let day = document.createElement('div')
+           if(i >= first_day.getDay()){
+            day.classList.add('calender-day-hover')
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>`
+            if( i - first_day.getDay() + 1 === currentDay.getDay() && currentDate.getFullYear() && month === currentDate.getMonth()){}
+                 day.classList.add('curr-date')
+           } 
+           calender_days.appendChild(day)
+    }
 }
+
+
+let currentDate = newDate()
+
+let currentMonth = {value:currentDate.month()}
+let currentYear = {value:currentDate.getFullYear()}
+
+generateCalender(currentMonth.value, currentYear.value)
