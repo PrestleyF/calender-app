85962175963b1dac4095e4cd51d9525aadd5113e
Prestley <priestlypriest@gmail.com>
2023-05-23 23:16:48 +0530

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 let nav = 0;
 let clicked = null;
-let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+const events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
 const newEventmodal = document.getElementById('newEventModal');
